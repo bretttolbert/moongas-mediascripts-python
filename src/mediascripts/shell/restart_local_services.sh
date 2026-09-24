dd06bae085a60597354acefa906a4076fe84cb2e
Brett Tolbert <bretttolbert@gmail.com>
2026-09-23 20:41:06 -0500

    wip

--- src/mediascripts/shell/restart_local_services.sh
+++ src/mediascripts/shell/restart_local_services.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if (( EUID != 0 )); then
+    echo "Error: This script must be run as root or with sudo." >&2
+    exit 1
+fi
+
 SVC=mediatunes-svc
 systemctl stop $SVC && sleep 1 && systemctl start $SVC; echo "Restarted $SVC"
 echo "Restarted $SVC"
